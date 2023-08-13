1f91339cc689bdaa9f0529982fdca974f4ab7c66
Jimmy <46508035+Jyrezo@users.noreply.github.com>
2023-08-13 19:22:45 +0000

    Fix Edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/RobloxTeethyz/Newsletter/',
+            'https://github.com/RobloxTeethyz/Newsletter/blob/main/issues/',
         },
 
         theme: {
